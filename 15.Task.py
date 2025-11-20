f06dd450c08dcb926561269b7c34e55fbd62ab1f
Shlomo <slomognatich@yandex.ru>
2025-11-20 15:09:50 +0300

    Task 15

--- 15.Task.py
+++ 15.Task.py
@@ -17,10 +17,9 @@ Numbers_ = [1,2,3,4,5,6,7,8]
 if Column_1 in Numbers_ and \
 Column_2 in Numbers_ and Row_1 in Numbers_ and Row_2 in Numbers_ :
 
-    if ((Column_1 + 1) < Column_2) or ((Column_1 - 1) > Column_2) or \
-        ((Row_1 + 1) < Row_2) or ((Row_1 - 1) > Row_2):
-        print('NO')
-    else :
+    if abs(Column_1 - Column_2) == abs(Row_1 - Row_2) :
         print('YES')
+    else :
+        print('NO')
 else :
     print('Некорректные координаты')
